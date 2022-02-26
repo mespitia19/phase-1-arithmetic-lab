86dc7aaedc70e843c61fe2ea2e0542f0eacfa9fb
MaximoEspitia <max19espitia@gmail.com>
2022-02-25 18:56:15 -0600

    commit tree ca59b8b14ce0512f692828f027e277a07588b1fd

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 12
+let num4 = 3
+const mod = num3/num4
+
+let max = Math.max(1,5,10,15,20)
